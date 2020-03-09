2b34f9f6835de18385b4e3733d8f25abf1660209
Малоземов Стас <srtx@mail.ru>
2020-03-09 15:15:48 +0500

    Пытаемся создать рабочую БД

---
+++ Code/ECMonitoring/ECMonitoring/Models/EcMonitoring/DBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ECMonitoring.Models.EcMonitoring
+{
+    public class DBContext : DbContext
+    {
+        public DBContext() : base("EcMonitoring")
+        { }
+
+        //public DbSet<Book> Books { get; set; }
+        //public DbSet<Purchase> Purchases { get; set; }
+    }
+}
