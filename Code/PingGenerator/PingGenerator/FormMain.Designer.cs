ef3ad62fe0c3eace6d22b71ef12e78077d280462
Малоземов Стас <srtx@mail.ru>
2020-03-15 19:05:41 +0500

    Ping Generstor и Диплом

---
+++ Code/PingGenerator/PingGenerator/FormMain.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace PingGenerator
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDstIP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPeriod = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "IP:";
+            // 
+            // txtDstIP
+            // 
+            this.txtDstIP.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDstIP.Location = new System.Drawing.Point(27, 37);
+            this.txtDstIP.Name = "txtDstIP";
+            this.txtDstIP.Size = new System.Drawing.Size(290, 20);
+            this.txtDstIP.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Период (ms):";
+            // 
+            // txtPeriod
+            // 
+            this.txtPeriod.Location = new System.Drawing.Point(27, 95);
+            this.txtPeriod.Name = "txtPeriod";
+            this.txtPeriod.Size = new System.Drawing.Size(118, 20);
+            this.txtPeriod.TabIndex = 4;
+            this.txtPeriod.Text = "300";
+            this.txtPeriod.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(182, 167);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStop.Location = new System.Drawing.Point(263, 167);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Стоп";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblStatus.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblStatus.Location = new System.Drawing.Point(0, 204);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(338, 39);
+            this.lblStatus.TabIndex = 8;
+            this.lblStatus.Text = "Статус";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 243);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPeriod);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDstIP);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDstIP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPeriod;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Code/PingGenerator/PingGenerator/FormMain.cs
@@ -0,0 +1,98 @@
+﻿using PingGenerator.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingGenerator
+{
+    public partial class FormMain : Form
+    {
+        System.Threading.Timer _timer { get; set; }
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            this.Text = Application.ProductName;
+            btnStop.Enabled = false;
+            txtDstIP.Text = Settings.Default.DstIp;
+            txtPeriod.Text = Settings.Default.Period.ToString();
+
+
+            
+            _timer = new System.Threading.Timer(PingTo);
+            //_timer.Tick += PingTo;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //_timer.Interval = Convert.ToInt32(txtPeriod.Text);
+            //_timer.Start();
+            _timer.Change(0, Convert.ToInt32(txtPeriod.Text));
+
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            _timer.Change(System.Threading.Timeout.Infinite, System.Threading.Timeout.Infinite);
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        //public void PingTo(object sender, EventArgs e)
+        public void PingTo(object o)
+        {
+            var address = txtDstIP.Text;
+            var errorMessage = string.Empty;
+            bool rez = false;
+            var ping = new Ping();
+            var pingReply = default(PingReply);
+
+            try
+            {
+                pingReply = ping.Send(address, 100);
+                rez = true;
+            }
+            catch(Exception ex)
+            {
+                errorMessage = ex.Message;
+            }
+            if (rez == true)
+            {
+                if (pingReply.Status == IPStatus.Success)
+                {
+                    lblStatus.Invoke(new Action(delegate { lblStatus.Text = "Пинг успешный."; }));
+                    lblStatus.Invoke(new Action(delegate { lblStatus.ForeColor = Color.FromArgb(0, 128, 0); }));
+                }
+                else
+                {
+                    errorMessage = "Пинг не успешный.";
+                }
+            }
+
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                lblStatus.Invoke(new Action(delegate { lblStatus.Text = $"Ошибка: {errorMessage}"; }));
+                lblStatus.Invoke(new Action(delegate { lblStatus.ForeColor = Color.FromArgb(255, 0, 0); }));
+            }
+
+            ping.Dispose();
+            ping = null; 
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Settings.Default.DstIp = this.txtDstIP.Text;
+            Settings.Default.Period = Convert.ToInt32(this.txtPeriod.Text);
+        }
+    }
+}
